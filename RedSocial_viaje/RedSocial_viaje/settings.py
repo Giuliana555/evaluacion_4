a2efc97d77ce16167cf455487f762399b803d390
kristel1920 <arielacofre23@gmail.com>
2024-12-17 19:34:56 -0300

    estilos de destino

--- RedSocial_viaje/RedSocial_viaje/settings.py
+++ RedSocial_viaje/RedSocial_viaje/settings.py
@@ -125,7 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'destinos/static'),  # Ruta para archivos estáticos
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
